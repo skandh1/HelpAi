ff1bb75c24869e18b90d1b3fd0b757e3eb11da8f
Skandh <Skandhjadon02@gmail.com>
2025-01-02 23:18:03 +0530

    make changes to to push code to vercel

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import Books from "./books.js";
+import Books from "../books.js";
 import dotenv from "dotenv";
 
 dotenv.config();
